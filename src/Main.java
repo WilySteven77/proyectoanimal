28101383c5995ef30f01d154925a1f65624ba2fc
WilySteven77 <78889326+WilySteven77@users.noreply.github.com>
2021-04-19 15:07:05 -0500

    Ejemplo de subir a git

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+    class person{
+        public String name = null;
+        public String Last_name = null;
+        public int age = 0;
+    }
+    public person(String name,String Last_name, int age){
+        this.name=name;
+        this.Last_name=Last_name;
+        this.age=age;
+    }
+    class Perro{
+    public String raza = null;
+    public int precio = 0;
+    }
+    public Perro{String raza, int precio){
+    this.raza=raza;
+    this.precio=precio;
+    }
+}
+    }
